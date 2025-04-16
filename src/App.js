5b4c6335ee94340a98b71e7e49445813ac888759
Mohammed Arif <20j41a1233@mrec.ac.in>
2025-04-17 01:09:48 +0530

    Setup for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import {NavBar} from './components/NavBar';
-import {Banner} from './components/Banner';
-import {Skills} from './components/Skills';
-import {Projects} from './components/Projects';
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter as Router } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact /> 
-     <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        <Contact /> 
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
